1e79b98c2f9bc0b56f2325a92974ad905123b93a
iwaokateppei <tep731kaizi@gmail.com>
2021-02-08 23:41:17 +0900

    create db

--- upgraded-blog/main.py
+++ upgraded-blog/main.py
@@ -1,57 +1,110 @@
-from pprint import pprint
-import smtplib
+from datetime import date
 
-from flask import Flask, render_template, request
-import requests
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+ckeditor = CKEditor(app)
+Bootstrap(app)
 
-api_endpoint = "https://api.npoint.io/43644ec4f0013682fc0d"
-blog_data = requests.get(api_endpoint).json()
-my_email = "XXX"
-password = "XXX"
-mail_to = "XXX"
+##CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+##CONFIGURE TABLE
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
 
 
-@app.route("/")
-def home():
-    return render_template("index.html", posts=blog_data)
+##WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    author = StringField("Your Name", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
 
+@app.route('/')
+def get_all_posts():
+    posts = db.session.query(BlogPost).all()
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/post/<int:num>")
+def show_post(num):
+    requested_post = None
+    posts = db.session.query(BlogPost).all()
+    for blog_post in posts:
+        if blog_post.id == num:
+            requested_post = blog_post
+    return render_template("post.html",data=requested_post)
+
+@app.route("/new_post", methods=["GET", "POST"])
+def create_new_post():
+        form = CreatePostForm()
+        if form.validate_on_submit():
+            new_post = BlogPost(
+                title=form.title.data,
+                subtitle=form.subtitle.data,
+                author=form.author.data,
+                img_url=form.img_url.data,
+                body=form.body.data,
+                date=date.today().strftime("%B %d, %Y")
+            )
+            db.session.add(new_post)
+            db.session.commit()
+            return redirect(url_for("get_all_posts"))
+        return render_template("make-post.html", form=form, is_edit=False)
+
+@app.route("/edit/<post_id>", methods=["GET", "POST"])
+def edit_post(post_id):
+    post = BlogPost.query.get(post_id)
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+    if edit_form.validate_on_submit():
+            post.title=edit_form.title.data
+            post.subtitle=edit_form.subtitle.data
+            post.author=edit_form.author.data
+            post.img_url=edit_form.img_url.data
+            post.body=edit_form.body.data
+            db.session.commit()
+            return redirect(url_for("get_all_posts"))
+    return render_template("make-post.html", form=edit_form, is_edit=True)
+
+@app.route("/delete/<post_id>")
+def delete_post(post_id):
+    post_to_delete = BlogPost.query.get(post_id)
+    db.session.delete(post_to_delete)
+    db.session.commit()
+    return redirect(url_for("get_all_posts"))
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
 
-@app.route("/contact", methods=["GET", "POST"])
+@app.route("/contact")
 def contact():
-    method_type = request.method
-    if method_type == 'POST':
-        form = request.form
-        name = form["name"]
-        mail = form["email"]
-        phone = form["phone"]
-        message = form["message"]
-        mail_content = f"Message from {name}, phone numes {phone}, email address {mail} \n {message}"
-        with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
-            connection.starttls()
-            connection.login(user=my_email, password=password)
-            connection.sendmail(from_addr=my_email,
-                                to_addrs=mail_to,
-                                msg=f'Subject:Message from your blog\n\n {mail_content}'
-                                )
-        return render_template("contact.html", msg_sent=True)
-    return render_template("contact.html", msg_sent=False)
-
-
-@app.route("/post/<num>")
-def post(num):
-    target_data = {}
-    for post in blog_data:
-        if post["id"] == int(num):
-            target_data = post
-    return render_template("post.html", data=target_data)
-
+    return render_template("contact.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=5000)
